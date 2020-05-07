b09861ee2c2b488140b7d6ac42500bdcec53a1c4
K00221314 <K00221314@student.lit.ie>
2020-05-07 01:33:03 +0100

    chang

--- assets.database/mathmania1.sql
+++ assets.database/mathmania1.sql
@@ -1,50 +1,87 @@
--- phpMyAdmin SQL Dump
--- version 4.8.3
--- https://www.phpmyadmin.net/
+CREATE DATABASE  IF NOT EXISTS `mathmania1` /*!40100 DEFAULT CHARACTER SET latin1 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `mathmania1`;
+-- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
 --
--- Host: 127.0.0.1
--- Generation Time: Mar 24, 2020 at 11:13 PM
--- Server version: 10.1.37-MariaDB
--- PHP Version: 7.2.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: mathmania1
+-- ------------------------------------------------------
+-- Server version	8.0.20
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Database: `mathmania1`
+-- Table structure for table `results`
 --
 
--- --------------------------------------------------------
+DROP TABLE IF EXISTS `results`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `results` (
+  `category` varchar(200) NOT NULL,
+  `type` varchar(200) NOT NULL,
+  `difficulty` varchar(200) NOT NULL,
+  `question` varchar(200) NOT NULL,
+  `correct_answer` varchar(200) NOT NULL,
+  `incorrect_answers1` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `results`
+--
+
+LOCK TABLES `results` WRITE;
+/*!40000 ALTER TABLE `results` DISABLE KEYS */;
+INSERT INTO `results` VALUES ('2','2','2','2','2','2'),('2','2','2','2','2','2');
+/*!40000 ALTER TABLE `results` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `uploads`
 --
 
+DROP TABLE IF EXISTS `uploads`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `uploads` (
-  `uploadId` int(20) NOT NULL,
+  `uploadId` int NOT NULL AUTO_INCREMENT,
   `image` varchar(200) NOT NULL,
   `title` varchar(200) NOT NULL,
   `description` varchar(200) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `Rating` int(5) NOT NULL DEFAULT '0'
+  `user_id` int NOT NULL,
+  `Rating` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`uploadId`),
+  KEY `fk_userid_uploads` (`user_id`),
+  CONSTRAINT `fk_userid_uploads` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `uploads`
+--
+
+LOCK TABLES `uploads` WRITE;
+/*!40000 ALTER TABLE `uploads` DISABLE KEYS */;
+/*!40000 ALTER TABLE `uploads` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
 --
 
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
-  `user_id` int(11) NOT NULL,
+  `user_id` int NOT NULL AUTO_INCREMENT,
   `f_name` text NOT NULL,
   `l_name` text NOT NULL,
   `email` varchar(50) NOT NULL,
@@ -52,64 +89,28 @@ CREATE TABLE `users` (
   `username` varchar(28) NOT NULL,
   `profile_pic` varchar(20) NOT NULL,
   `password` varchar(20) NOT NULL,
-  `bio` varchar(500) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `bio` varchar(500) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`user_id`, `f_name`, `l_name`, `email`, `AccountType`, `username`, `profile_pic`, `password`, `bio`) VALUES
-(12, 'Admin', 'Admin', 'c@c.c', 'Admin', 'Admin', '1.jpg', 'Admin', 'wwwwwwwwwwwww'),
-(17, 'Q', 'Q', 'Q@Q', 'Student', 'Q', '1.jpg', 'Q', 'QTRRTRSRHSRHERH'),
-(21, 'qqq', 'q', 'q@qqqqqq', 'Student', 'qqqqq', '1.jpg', 'q', 'qqqqq'),
-(22, 't', 't', 't@ttttt', 'Student', 'ttttttrrrrrrrr', '1.jpg', 't', 'tttttttttttttttttttttttttttt'),
-(24, 'eeee', 'eeeee', 'e@e', 'Student', 'e', '1.jpg', 'e', 'eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee');
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (12,'Admin','Admin','c@c.c','Admin','Admin','1.jpg','Admin','wwwwwwwwwwwww'),(17,'Q','Q','Q@Q','Student','Q','1.jpg','Q','QTRRTRSRHSRHERH'),(21,'qqq','q','q@qqqqqq','Student','qqqqq','1.jpg','q','qqqqq'),(22,'t','t','t@ttttt','Student','ttttttrrrrrrrr','1.jpg','t','tttttttttttttttttttttttttttt'),(24,'eeee','eeeee','e@e','Student','e','1.jpg','e','eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `uploads`
---
-ALTER TABLE `uploads`
-  ADD PRIMARY KEY (`uploadId`),
-  ADD KEY `fk_userid_uploads` (`user_id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `uploads`
---
-ALTER TABLE `uploads`
-  MODIFY `uploadId` int(20) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `uploads`
---
-ALTER TABLE `uploads`
-  ADD CONSTRAINT `fk_userid_uploads` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-05-07  1:32:50
