c2ac3c52888452bd4a8f87e3b3f235ac07a2db38
K00221314 <K00221314@student.lit.ie>
2020-05-20 01:21:23 +0100

    Adding Meta Data Class Managers

---
+++ src/java/controller/WebsiteMap.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+/**
+ *
+ * @author Rob
+ */
+public class WebsiteMap
+{
+	public final static String AddUser = "AddUser.jsp";
+	public final static String ManageResults = "manageResults.jsp";
+}
